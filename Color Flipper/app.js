580c0342083c157e9618ca9d8661dda18d77582d
Vishal Kandu <88443773+vishalkandu01@users.noreply.github.com>
2022-06-23 17:09:59 +0530

    Add files via upload

---
+++ Color Flipper/app.js
@@ -0,0 +1,20 @@
+const colors = ["green", "pink", "yellow", "rgba(133, 122, 200)", "#f15025"];
+// const colors = ["#f12323", "#f69263", "#f35343", "#f34234", "grey", "pink"];
+const btn = document.getElementById('btn');
+const color = document.querySelector(".color");
+
+btn.addEventListener('click', function(){
+    // get random number between 0 - 3 
+    const randomNumber = getRandomNumber();
+    console.log(randomNumber);
+
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];       // The textContent property sets or returns the text content of the specified node, and all its descendants.
+})
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * colors.length);
+}
+
+// console.log(document.body);
+// console.log(document.head);
