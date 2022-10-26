c3ce2168d4e3bdaa193076db7bf9b819cb6c6489
suraj mishra <soorajmishra2000@gmail.com>
2022-10-26 11:21:14 +0530

    js page is changed

--- Color Flipper/app.js
+++ Color Flipper/app.js
@@ -2,9 +2,13 @@ const colors = ["green", "pink", "yellow", "rgba(133, 122, 200)", "#f15025"];
 // const colors = ["#f12323", "#f69263", "#f35343", "#f34234", "grey", "pink"];
 const btn = document.getElementById('btn');
 const color = document.querySelector(".color");
-
+btn.addEventListener('onclick',function(){
+    console.log("color changed");
+})
 btn.addEventListener('click', function(){
     // get random number between 0 - 3 
+    
+alert("you are changing the bg color");
     const randomNumber = getRandomNumber();
     console.log(randomNumber);
 
